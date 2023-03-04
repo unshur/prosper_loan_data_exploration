3920c0555cb75c66d8979e3968636a923eefc56f
Ahmed Unshur <unshuramh@gmail.com>
2023-03-04 15:41:55 +0200

    Add `libraries.py` to repository

---
+++ libraries.py
@@ -0,0 +1,12 @@
+# Import required packages.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+# Enable inline plotting.
+%matplotlib inline
+
+# Ignore warnings.
+import warnings
+warnings.simplefilter("ignore")
